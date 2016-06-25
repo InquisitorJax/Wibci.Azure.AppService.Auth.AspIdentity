341e2623980ed3501ee9163fa0d2ce58cf8085be
Malcolm Jack <inquisitorjax@gmail.com>
2016-06-25 12:29:54 +0200

    add skeleton projects for xamarin

---
+++ Wibci.Azure.AppService.Auth.XamarinClient/Wibci.Azure.AppService.Auth.XamarinClient.Droid/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Wibci.Azure.AppService.Auth.XamarinClient.Droid
+{
+    [Activity(Label = "Wibci.Azure.AppService.Auth.XamarinClient", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+    }
+}
+
